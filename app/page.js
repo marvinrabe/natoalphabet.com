ac85e420cdf0d939d7f9d336d1f4cf60980f2c64
Marvin Rabe <marvin.rabe@gmail.com>
2024-03-11 11:38:30 +0100

    feat: basic nato alphabet table design

--- app/page.js
+++ app/page.js
@@ -2,112 +2,167 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    <main className="flex flex-col mx-auto w-full max-w-[640px] p-8">
+      <h1 className="text-2xl mb-8 flex items-center gap-2">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          className=""
+          src="/logo.svg"
+          alt="NATO Alphabet"
+          width={48}
+          height={48}
           priority
         />
+        <span className="font-bold">NATO</span> Alphabet
+      </h1>
+      <div className="bg-white dark:bg-gray-900 border border-gray-300 dark:border-gray-800 shadow-2xl rounded-md py-2 px-4">
+        <table className="w-full text-left">
+          <caption className="sr-only">
+            NATO Phonetic Alphabet
+          </caption>
+          <thead className="border-b-2 border-gray-300 dark:border-gray-800">
+            <tr>
+              <th className="font-normal text-gray-600 dark:text-gray-300">Symbol</th>
+              <th className="font-normal text-gray-600 dark:text-gray-300">Code Word</th>
+              <th className="font-normal text-gray-600 dark:text-gray-300">Morse Code</th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-300 dark:divide-gray-800">
+            <tr>
+              <th>A</th>
+              <td><strong>A</strong>lfa</td>
+              <td>● ▬</td>
+            </tr>
+            <tr>
+              <th>B</th>
+              <td><strong>B</strong>ravo</td>
+              <td>▬ ● ● ●</td>
+            </tr>
+            <tr>
+              <th>C</th>
+              <td><strong>C</strong>harlie</td>
+              <td>▬ ● ▬ ●</td>
+            </tr>
+            <tr>
+              <th>D</th>
+              <td><strong>D</strong>elta</td>
+              <td>▬ ● ●</td>
+            </tr>
+            <tr>
+              <th>E</th>
+              <td><strong>E</strong>cho</td>
+              <td>●</td>
+            </tr>
+            <tr>
+              <th>F</th>
+              <td><strong>F</strong>oxtrot</td>
+              <td>● ● ▬ ●</td>
+            </tr>
+            <tr>
+              <th>G</th>
+              <td><strong>G</strong>olf</td>
+              <td>▬ ▬ ●</td>
+            </tr>
+            <tr>
+              <th>H</th>
+              <td><strong>H</strong>otel</td>
+              <td>● ● ● ●</td>
+            </tr>
+            <tr>
+              <th>I</th>
+              <td><strong>I</strong>ndia</td>
+              <td>● ●</td>
+            </tr>
+            <tr>
+              <th>J</th>
+              <td><strong>J</strong>uliett</td>
+              <td>● ▬ ▬ ▬</td>
+            </tr>
+            <tr>
+              <th>K</th>
+              <td><strong>K</strong>ilo</td>
+              <td>▬ ● ▬</td>
+            </tr>
+            <tr>
+              <th>L</th>
+              <td><strong>L</strong>ima</td>
+              <td>● ▬ ● ●</td>
+            </tr>
+            <tr>
+              <th>M</th>
+              <td><strong>M</strong>ike</td>
+              <td>▬ ▬</td>
+            </tr>
+            <tr>
+              <th>N</th>
+              <td><strong>N</strong>ovember</td>
+              <td>▬ ●</td>
+            </tr>
+            <tr>
+              <th>O</th>
+              <td><strong>O</strong>scar</td>
+              <td>▬ ▬ ▬</td>
+            </tr>
+            <tr>
+              <th>P</th>
+              <td><strong>P</strong>apa</td>
+              <td>● ▬ ▬ ●</td>
+            </tr>
+            <tr>
+              <th>Q</th>
+              <td><strong>Q</strong>uebec</td>
+              <td>▬ ▬ ● ▬</td>
+            </tr>
+            <tr>
+              <th>R</th>
+              <td><strong>R</strong>omeo</td>
+              <td>● ▬ ●</td>
+            </tr>
+            <tr>
+              <th>S</th>
+              <td><strong>S</strong>ierra</td>
+              <td>● ● ●</td>
+            </tr>
+            <tr>
+              <th>T</th>
+              <td><strong>T</strong>ango</td>
+              <td>▬</td>
+            </tr>
+            <tr>
+              <th>U</th>
+              <td><strong>U</strong>niform</td>
+              <td>● ● ▬</td>
+            </tr>
+            <tr>
+              <th>V</th>
+              <td><strong>V</strong>ictor</td>
+              <td>● ● ● ▬</td>
+            </tr>
+            <tr>
+              <th>W</th>
+              <td><strong>W</strong>hiskey</td>
+              <td>● ▬ ▬</td>
+            </tr>
+            <tr>
+              <th>X</th>
+              <td><strong>X</strong>-ray</td>
+              <td>▬ ● ● ▬</td>
+            </tr>
+            <tr>
+              <th>Y</th>
+              <td><strong>Y</strong>ankee</td>
+              <td>▬ ● ▬ ▬</td>
+            </tr>
+            <tr>
+              <th>Z</th>
+              <td><strong>Z</strong>ulu</td>
+              <td>▬ ▬ ● ●</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <p className="text-xs pt-8 text-gray-400 dark:text-gray-600">
+        Compass Rose by NAPISAH from <a href="https://thenounproject.com/browse/icons/term/compass-rose/" target="_blank" title="Compass Rose Icons">Noun Project</a> (CC BY 3.0)
+      </p>
     </main>
   );
 }
